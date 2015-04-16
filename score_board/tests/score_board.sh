7afe935b657033757619c2ecf0bead8eb5cd7506
René Kjellerup <rk.katana.steel@gmail.com>
2015-04-15 18:49:30 -0700

    added 5 tests for UDP against ScoreBoard

--- score_board/tests/score_board.sh
+++ score_board/tests/score_board.sh
@@ -6,12 +6,46 @@ function fin_exit() {
   exit $1
 }
 
-nc -lui 10 0.0.0.0 9500 > master.log 2>&1 &
+function test_running() {
+  ps ax | grep $SB > /dev/null 2>&1
+}
+
+nc -lui 3 -p 9500 > master.log 2>&1 &
 
 ./score_board &
 SB=$!
 
-sleep 10
+sleep 5
+P=$(cat master.log|awk '{print $2}')
+[[ -z $P ]] && echo "Failed to send needUi Request" && fin_exit 1
+echo "Got Port ${P}"
 
-echo "All Pass!"
+echo -n "Timer test 1 fake input x<space>"
+echo "timer x " | nc -uq 1 127.0.0.1 $P
+test_running || fin_exit 1
+echo " Passed!"
+
+echo -n "Timer test 2 fake input non-integers and wrong format"
+echo "timer x y z" | nc -uq 1 127.0.0.1 $P
+test_running || fin_exit 1
+echo " Passed!"
+
+echo -n "Timer test 3 fake input non-integers"
+echo "timer x MM:SS:%%" | nc -uq 1 127.0.0.1 $P
+test_running || fin_exit 1
+echo " Passed!"
+
+echo -n "Timer test 4 start 6:0:0"
+echo "timer start 6:0:0" | nc -uq 1 127.0.0.1 $P
+test_running || fin_exit 1
+echo " Passed!"
+
+echo -n "Timer test 5 stop"
+echo "timer stop" | nc -uq 1 127.0.0.1 $P
+test_running || fin_exit 1
+echo " Passed!"
+
+
+
+echo "All Passed!"
 fin_exit $?
