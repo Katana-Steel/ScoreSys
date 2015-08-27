2e28e565b39d217d5ccbe46f13732270e803911a
Rene Kjellerup <rk.katana.steel@gmail.com>
2015-08-27 14:05:59 -0700

    trying to update karate scores

--- score_board/karate.cpp
+++ score_board/karate.cpp
@@ -13,10 +13,28 @@ void
 Karate::setRightPlayer(QList<QString> pldata)
 {
   nameAka->setText (pldata.at (0));
+  scoreAka->setValue (pldata.at (1).toInt());
+  try {
+    aka_cat1->setText  (pldata.at (2));
+    aka_cat2->setText  (pldata.at (3));
+  }
+  catch (...)
+  {
+    //done
+  }
 }
 
 void
 Karate::setLeftPlayer(QList<QString> pldata)
 {
   nameAo->setText (pldata.at (0));
+  scoreAka->setValue (pldata.at (1).toInt());
+  try {
+    aka_cat1->setText  (pldata.at (2));
+    aka_cat2->setText  (pldata.at (3));
+  }
+  catch (...)
+  {
+    //done
+  }
 }
