2551b3ed4f2dcdd9ce79b1df36c79f7676f4c181
Rene Kjellerup <rk.katana.steel@gmail.com>
2015-08-30 16:36:18 -0700

    removed strict dependency on traditional netcat from test script

--- score_board/tests/score_board.sh
+++ score_board/tests/score_board.sh
@@ -10,6 +10,16 @@ function test_running() {
   ps ax | grep $SB > /dev/null 2>&1
 }
 
+function set_trad_nc() {
+  NCSERV="nc -lui 3 -p 9500"
+  NCCLI="nc -uq 1"
+}
+
+function set_bsd_nc() {
+  NCSERV="nc -l -p 9500 -u -i 3"
+  NCCLI="nc -u -q 1"
+}
+
 grep = Makefile | head -9 | tee tests/Makefile
 cat tests/makefile.build >> tests/Makefile
 cd tests
@@ -18,7 +28,14 @@ cd -
 
 tests/sboard_test || fin_exit $?
 
-nc -lui 3 -p 9500 > master.log 2>&1 &
+nc -h 2>&1 | head -1 | grep -i bsd &> /dev/null
+if [ $? -eq 0 ]; then
+  set_bsd_nc
+else
+  set_trad_nc
+fi
+
+$NCSERV &> master.log &
 
 ./score_board &
 SB=$!
@@ -29,42 +46,42 @@ P=$(cat master.log|awk '{print $2}')
 echo "Got Port ${P}"
 
 echo -n "Timer test 1 fake input x<space>"
-echo "timer x " | nc -uq 1 127.0.0.1 $P
+echo "timer x " | $NCCLI 127.0.0.1 $P
 test_running || fin_exit 1
 echo " Passed!"
 
 echo -n "Timer test 2 fake input non-integers and wrong format"
-echo "timer x y z" | nc -uq 1 127.0.0.1 $P
+echo "timer x y z" | $NCCLI 127.0.0.1 $P
 test_running || fin_exit 1
 echo " Passed!"
 
 echo -n "Timer test 3 fake input non-integers"
-echo "timer x MM:SS:%%" | nc -uq 1 127.0.0.1 $P
+echo "timer x MM:SS:%%" | $NCCLI 127.0.0.1 $P
 test_running || fin_exit 1
 echo " Passed!"
 
 echo -n "Timer test 4 start 6:0:0"
-echo "timer start 6:0:0" | nc -uq 1 127.0.0.1 $P
+echo "timer start 6:0:0" | $NCCLI 127.0.0.1 $P
 test_running || fin_exit 1
 echo " Passed!"
 
 echo -n "Timer test 5 stop"
-echo "timer stop" | nc -uq 1 127.0.0.1 $P
+echo "timer stop" | $NCCLI 127.0.0.1 $P
 test_running || fin_exit 1
 echo " Passed!"
 
 echo -n "Pre Ui player update 1"
-echo "player r 'Claude Moné' pt:4 cat1:0 cat2:1" | nc -uq 1 127.0.0.1 $P
+echo "player r 'Claude Moné' pt:4 cat1:0 cat2:1" | $NCCLI 127.0.0.1 $P
 test_running || fin_exit 1
 echo " Passed!"
 
 echo -n "Set Ui test 1"
-echo "setUi 'Killer Floor' karate" | nc -uq 1 127.0.0.1 $P
+echo "setUi 'Killer Floor' karate" | $NCCLI 127.0.0.1 $P
 test_running || fin_exit 1
 echo " Passed!"
 
 echo -n "Player update test 2 (post ui)"
-echo "player r 'Claude Moné' pt:3 cat1:0 cat2:1" | nc -uq 1 127.0.0.1 $P
+echo "player r 'Claude Moné' pt:3 cat1:0 cat2:1" | $NCCLI 127.0.0.1 $P
 test_running || fin_exit 1
 echo " Passed!"
 
