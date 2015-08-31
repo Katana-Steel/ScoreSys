48ec10f31cbae52fc3b165c68580e7290068384a
Rene Kjellerup <rk.katana.steel@gmail.com>
2015-08-30 23:11:50 -0700

    test refactoring, and more failure checking

--- score_board/tests/score_board.sh
+++ score_board/tests/score_board.sh
@@ -10,6 +10,13 @@ function test_running() {
   ps ax | grep $SB > /dev/null 2>&1
 }
 
+function cli_test () {
+  local cmd=$1
+  local port=$2
+  echo $cmd | $NCCLI 127.0.0.1 $port || fin_exit 2
+  test_running || fin_exit 1
+}
+
 function set_trad_nc() {
   NCSERV="nc -lui 3 -p 9500"
   NCCLI="nc -uq 1"
@@ -34,8 +41,10 @@ if [ $? -eq 0 ]; then
 else
   set_trad_nc
 fi
+echo "fake server:      $NCSERV"
+echo "fake server cli:  $NCCLI"
 
-$NCSERV &> master.log &
+$NCSERV > master.log &
 
 ./score_board &
 SB=$!
@@ -46,43 +55,35 @@ P=$(tail -1 master.log|awk '{print $2}')
 echo "Got Port ${P}"
 
 echo -n "Timer test 1 fake input x<space>"
-echo "timer x " | $NCCLI 127.0.0.1 $P
-test_running || fin_exit 1
+cli_test "timer x " "$P"
 echo " Passed!"
 
 echo -n "Timer test 2 fake input non-integers and wrong format"
-echo "timer x y z" | $NCCLI 127.0.0.1 $P
-test_running || fin_exit 1
+cli_test "timer x y z" "$P"
 echo " Passed!"
 
 echo -n "Timer test 3 fake input non-integers"
-echo "timer x MM:SS:%%" | $NCCLI 127.0.0.1 $P
-test_running || fin_exit 1
+cli_test "timer x MM:SS:%%" "$P"
 echo " Passed!"
 
 echo -n "Timer test 4 start 6:0:0"
-echo "timer start 6:0:0" | $NCCLI 127.0.0.1 $P
-test_running || fin_exit 1
+cli_test "timer start 6:0:0" "$P"
 echo " Passed!"
 
 echo -n "Timer test 5 stop"
-echo "timer stop" | $NCCLI 127.0.0.1 $P
-test_running || fin_exit 1
+cli_test "timer stop" "$P"
 echo " Passed!"
 
 echo -n "Pre Ui player update 1"
-echo "player r 'Claude Moné' pt:4 cat1:0 cat2:1" | $NCCLI 127.0.0.1 $P
-test_running || fin_exit 1
+cli_test "player r 'Claude Moné' pt:4 cat1:0 cat2:1" "$P"
 echo " Passed!"
 
 echo -n "Set Ui test 1"
-echo "setUi 'Killer Floor' karate" | $NCCLI 127.0.0.1 $P
-test_running || fin_exit 1
+cli_test "setUi 'Killer Floor' karate" $P
 echo " Passed!"
 
 echo -n "Player update test 2 (post ui)"
-echo "player r 'Claude Moné' pt:3 cat1:0 cat2:1" | $NCCLI 127.0.0.1 $P
-test_running || fin_exit 1
+cli_test "player r 'Claude Moné' pt:3 cat1:0 cat2:1" "$P"
 echo " Passed!"
 
 echo "All Passed!"
