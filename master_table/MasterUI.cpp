8204d01300a65bae679f04e01631c74db4bd1fed
Rene Kjellerup <rk.katana.steel@gmail.com>
2015-09-07 11:44:09 -0700

    added master server to the UI dialog

--- master_table/MasterUI.cpp
+++ master_table/MasterUI.cpp
@@ -1,9 +1,16 @@
 #include  <QtGui>
 #include "MasterUI.h"
+#include "MasterServer.h"
+#include "board.h"
 
-
-MainMasterUi:: MainMasterUi(QWidget *parent)
+MainMasterUi::MainMasterUi(QWidget *parent)
   : QMainWindow (parent)
 { 
   setupUi(this);
-} 
+  this->server =  new MasterServer();
+}
+
+void
+MainMasterUi::addNewBoard()
+{
+}
