0bf71ccd8f5dc56d2ab1b14508560129baa8f7fb
Rene Kjellerup <rk.katana.steel@gmail.com>
2015-09-08 15:25:27 -0700

    deferred setup of judge table  and score board units

--- common_lib/unit.cpp
+++ common_lib/unit.cpp
@@ -11,7 +11,4 @@ Unit::Unit(QObject *parent)
 
 void Unit::addScoreBoard(quint64 id)
 {
-    Board *brd = new Board(id);
-    boards.append(brd);
-    connect(table, SIGNAL(exec(const QString&)),brd,SLOT(execCommand(const QString&)));
 }
