5ec4812b38a3133812e25b7af1d7c28beb4491cc
Rene Kjellerup <rk.katana.steel@gmail.com>
2015-09-08 08:21:17 -0700

    loading the master server ui

--- master_table/main.cpp
+++ master_table/main.cpp
@@ -2,12 +2,17 @@
 
 #include <QApplication>
 #include "test.h"
+#include "MasterUI.h"
 
 int main(int ac, char** av)
 {
     QApplication a(ac,av);
     try {
         test();
+        if (! a.arguments().contains ("--test-only")) { 
+            MainMasterUi *main = new MainMasterUi();
+            main->show();
+        }
     }
     catch(...) {
         return -1;
